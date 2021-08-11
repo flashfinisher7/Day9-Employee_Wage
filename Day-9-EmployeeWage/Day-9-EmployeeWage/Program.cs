7212e42f5551ad2a6fe80e2183e711d314d58bfe
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-11 15:12:16 +0530

    Refactor: Employee Wage

---
+++ Day-9-EmployeeWage/Day-9-EmployeeWage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day_9_EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Day_9_EmployeeWage.EmployeeClass obj1 = new Day_9_EmployeeWage.EmployeeClass();
+            obj1.CompanyEmpWage("Wipro", 57, 27, 100);
+            obj1.CompanyEmpWage("TCS", 77, 30, 150);
+        }
+    }
+}
